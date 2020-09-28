1398a5a2226002f91b666e0d39f94040f7a9cb86
Nicolas Hurtado <nhd15c@gmail.com>
2020-09-28 19:49:54 -0400

    Added colors to console logs.

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const net = require('net');
+const colors = require('colors/safe');
 
 // ENVVARs.
 const socketPort = process.env.PORT || 8124;
 
+// Utils.
+const serverString = colors.yellow('[SERVER]');
+const clientString = colors.green('[CLIENT]');
+
 // Creating the server.
 const server = net.createServer((tcpSocket) => {
-  console.log('Client connected.');
+  console.log(`${clientString} Client connected.`);
 
   tcpSocket.write('Sucessful connection.\n');
 
@@ -14,12 +19,12 @@ const server = net.createServer((tcpSocket) => {
   }, 5000);
 
   tcpSocket.on('end', () => {
-    console.log('Client disconnected.');
+    console.log(`${clientString} Client disconnected.`);
     clearInterval(cTimer);
   });
 });
 
 // Binding and starting the server.
 server.listen(socketPort, '127.0.0.1', 1, () => {
-  console.log(`Established. Listening on ${socketPort}`)
+  console.log(`${serverString} Connected. Listening on ${socketPort}.`)
 });
