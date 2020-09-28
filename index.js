d77b1e1d8c4395326a05b50f14778dced77ccb72
Nicolas Hurtado <nhd15c@gmail.com>
2020-09-28 18:57:04 -0400

    Setup basic TCP socket to listen for connections

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Hello world');
+const net = require('net');
+
+// ENVVARs.
+const socketPort = process.env.PORT || 8124;
+
+// Creating the server.
+const server = net.createServer(() => {
+  console.log('Client connected.');
+});
+
+// Binding and starting the server.
+server.listen(socketPort, '127.0.0.1', 1, () => {
+  console.log(`Established. Listening on ${socketPort}`)
+});
