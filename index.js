f46afa779f2cd3be1740c0679a0b5ba6c0d6f824
Nicolas Hurtado <nhd15c@gmail.com>
2020-09-29 00:00:54 -0400

    Data can now be read from the socket

--- index.js
+++ index.js
@@ -5,21 +5,34 @@ const colors = require('colors/safe');
 const socketPort = process.env.PORT || 8124;
 
 // Utils.
-const serverString = colors.yellow('[SERVER]');
-const clientString = colors.green('[CLIENT]');
+const serverStr = colors.yellow('[SERVER]');
+const clientStr = colors.green('[CLIENT]');
 
 // Creating the server.
 const server = net.createServer((tcpSocket) => {
-  console.log(`${clientString} (${tcpSocket.remoteAddress}:${tcpSocket.remotePort}) connected.`);
-
+  // Notifications.
+  const clientIdStr = `(${tcpSocket.remoteAddress}:${tcpSocket.remotePort})`;
+  console.log(`${clientStr} ${clientIdStr} connected.`);
   tcpSocket.write('Sucessful connection.\n');
 
+  // Handle connection closed.
   tcpSocket.on('end', () => {
-    console.log(`${clientString} (${tcpSocket.remoteAddress}:${tcpSocket.remotePort}) disconnected.`);
+    console.log(`${clientStr} ${clientIdStr} disconnected.`);
   });
+
+  // Handle data.
+  tcpSocket.on('data', (dataSent) => {
+    console.log(`${clientStr} Data received from ${clientIdStr}.`);
+
+    console.log(dataSent.toString('utf-8'));
+
+    tcpSocket.write(`You sent this: ${dataSent.toString('utf-8')}`);
+  });
+
+
 });
 
 // Binding and starting the server.
 server.listen(socketPort, '127.0.0.1', 1, () => {
-  console.log(`${serverString} Connected. Listening on ${socketPort}.`)
+  console.log(`${serverStr} Connected. Listening on ${socketPort}.`)
 });
