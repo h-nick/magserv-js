6bf2cf114ee17dd7a6033c13cf38972b5fb028be
Nicolas Hurtado <nhd15c@gmail.com>
2020-09-30 01:16:19 -0400

    Implemented Chapter 6 dictionary commands

--- http_server.js
+++ http_server.js
@@ -3,7 +3,8 @@ const colors = require('colors/safe');
 const TCPSocket = require('./tcp_socket');
 
 module.exports = class HttpServer {
-  #host; #port; #responses;
+  #host; #port;
+  #responses = new Map();
   #str = {
     server: colors.yellow('[SERVER]'),
     client: colors.brightRed('[CLIENT]')
@@ -16,7 +17,7 @@ module.exports = class HttpServer {
 
   #endHandler = (socket) => {
     try {
-      socket.internal.write('Finished. Bye!');
+      socket.internal.write('Finished. Bye!\r\n');
       socket.internal.destroy();
     } catch (error) {
       console.log(error);
@@ -25,8 +26,72 @@ module.exports = class HttpServer {
     console.log(`${this.#str.client} (${socket.addr}:${socket.port}) disconnected.`);
   }
 
+  #cmdGet = (word) => {
+    return this.#responses.get(word);
+  }
+
+  #cmdSet = (word, desc) => {
+    return this.#responses.set(word, desc);
+  }
+
+  #cmdClear = () => {
+    this.#responses.clear();
+  }
+
+  #cmdAll = () => {
+    const tmpArr = Array.from(this.#responses.keys());
+
+    return tmpArr.join(', ');
+  }
+
   #dataHandler = (socket, data) => {
     console.log(`${this.#str.client} (${socket.addr}:${socket.port}) sent data.`);
+
+    const stringMatch = data.toString('utf-8').match(/[^\s]+/g);
+
+    if (!stringMatch) {
+      socket.internal.write('ERROR A command was expected.\r\n');
+      return;
+    }
+
+    switch (stringMatch[0]) {
+      case 'GET':
+        if (!stringMatch[1]) {
+          socket.internal.write('ERROR Expected format is: GET <word>.\r\n');
+        }
+
+        const rVal = this.#cmdGet(stringMatch[1]);
+
+        if (rVal) {
+          socket.internal.write(`ANSWER ${rVal}\r\n`);
+        } else {
+          socket.internal.write(`ERROR Could not find the specified word.\r\n`);
+        }
+        break;
+
+      case 'SET':
+        const extraParam = stringMatch.slice(2).join(' ');
+
+        if (!stringMatch[1] || !extraParam) {
+          socket.internal.write('ERROR Expected format is: SET <word> <desc>\r\n');
+        }
+
+        this.#cmdSet(stringMatch[1], extraParam);
+        socket.internal.write(`ANSWER Word ${stringMatch[1]} has been set.\r\n`);
+        break;
+
+      case 'CLEAR':
+        this.#cmdClear();
+        socket.internal.write(`ANSWER The dictionary has been cleared.\r\n`);
+        break;
+
+      case 'ALL':
+        socket.internal.write(`ANSWER Available words: ${this.#cmdAll()}.\r\n`);
+        break;
+
+      default:
+        socket.internal.write(`ERROR Command not found.\r\n`);
+    }
   }
 
   #tcpHandler = (client) => {
@@ -34,7 +99,7 @@ module.exports = class HttpServer {
     console.log(`${this.#str.client} (${socket.addr}:${socket.port}) connected.`);
 
     socket.internal.write('Sucessful connection.\r\n');
-    socket.internal.write('Available commands: GET, SET, CLEAR, ALL');
+    socket.internal.write('Available commands: GET, SET, CLEAR, ALL.\r\n');
 
     socket.internal.on('end', () => this.#endHandler(socket));
     socket.internal.on('data', (data) => this.#dataHandler(socket, data));
