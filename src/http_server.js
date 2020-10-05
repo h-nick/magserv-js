6cbeccb7f10f764ec969dca49c4d5e854ccdde2b
Nicolas Hurtado <nhd15c@gmail.com>
2020-10-05 02:43:06 -0400

    Added appropriate try-catch blocks

--- src/http_server.js
+++ src/http_server.js
@@ -51,78 +51,90 @@ module.exports = class HttpServer {
   }
 
   #dataHandler = (socket, data) => {
-    console.log(`${this.#str.client} (${socket.addr}:${socket.port}) sent data.`);
+    try {
+      console.log(`${this.#str.client} (${socket.addr}:${socket.port}) sent data.`);
 
-    const stringMatch = data.toString('utf-8').match(/[^\s]+/g);
+      const stringMatch = data.toString('utf-8').match(/[^\s]+/g);
 
-    if (!stringMatch) {
-      socket.internal.write('ERROR A command was expected.\r\n');
-      return;
-    }
+      if (!stringMatch) {
+        socket.internal.write('ERROR A command was expected.\r\n');
+        return;
+      }
 
-    switch (stringMatch[0]) {
-      case 'GET': {
-        if (!stringMatch[1]) {
-          socket.internal.write('ERROR Expected format is: GET <word>.\r\n');
+      switch (stringMatch[0]) {
+        case 'GET': {
+          if (!stringMatch[1]) {
+            socket.internal.write('ERROR Expected format is: GET <word>.\r\n');
+            break;
+          }
+
+          const rVal = this.#cmdGet(stringMatch[1]);
+
+          if (rVal) {
+            socket.internal.write(`ANSWER ${rVal}\r\n`);
+          } else {
+            socket.internal.write('ERROR Could not find the specified word.\r\n');
+          }
           break;
         }
 
-        const rVal = this.#cmdGet(stringMatch[1]);
+        case 'SET': {
+          const extraParam = stringMatch.slice(2).join(' ');
 
-        if (rVal) {
-          socket.internal.write(`ANSWER ${rVal}\r\n`);
-        } else {
-          socket.internal.write('ERROR Could not find the specified word.\r\n');
-        }
-        break;
-      }
+          if (!stringMatch[1] || !extraParam) {
+            socket.internal.write('ERROR Expected format is: SET <word> <desc>\r\n');
+            break;
+          }
 
-      case 'SET': {
-        const extraParam = stringMatch.slice(2).join(' ');
-
-        if (!stringMatch[1] || !extraParam) {
-          socket.internal.write('ERROR Expected format is: SET <word> <desc>\r\n');
+          this.#cmdSet(stringMatch[1], extraParam);
+          socket.internal.write(`ANSWER Word ${stringMatch[1]} has been set.\r\n`);
           break;
         }
 
-        this.#cmdSet(stringMatch[1], extraParam);
-        socket.internal.write(`ANSWER Word ${stringMatch[1]} has been set.\r\n`);
-        break;
-      }
-
-      case 'CLEAR': {
-        this.#cmdClear();
-        socket.internal.write('ANSWER The dictionary has been cleared.\r\n');
-        break;
-      }
-
-      case 'ALL': {
-        const words = this.#cmdAll();
-
-        if (words) {
-          socket.internal.write(`ANSWER Available words: ${words}.\r\n`);
-        } else {
-          socket.internal.write('ERROR There are no words saved.\r\n');
+        case 'CLEAR': {
+          this.#cmdClear();
+          socket.internal.write('ANSWER The dictionary has been cleared.\r\n');
+          break;
         }
-        break;
+
+        case 'ALL': {
+          const words = this.#cmdAll();
+
+          if (words) {
+            socket.internal.write(`ANSWER Available words: ${words}.\r\n`);
+          } else {
+            socket.internal.write('ERROR There are no words saved.\r\n');
+          }
+          break;
+        }
+
+        default:
+          socket.internal.write('ERROR Command not found.\r\n');
       }
 
-      default:
-        socket.internal.write('ERROR Command not found.\r\n');
+      socket.internal.write('---------------\r\n');
+    } catch (error) {
+      console.log(
+        colors.brightRed(`(${socket.addr}:${socket.port}) d/c on data listener.`),
+      );
     }
-
-    socket.internal.write('---------------\r\n');
   }
 
   #tcpHandler = (client) => {
     const socket = new TCPSocket(client);
-    console.log(`${this.#str.client} (${socket.addr}:${socket.port}) connected.`);
+    try {
+      console.log(`${this.#str.client} (${socket.addr}:${socket.port}) connected.`);
 
-    socket.internal.write('Successful connection.\r\n');
-    socket.internal.write('Available commands: GET, SET, CLEAR, ALL.\r\n');
+      socket.internal.write('Successful connection.\r\n');
+      socket.internal.write('Available commands: GET, SET, CLEAR, ALL.\r\n');
 
-    socket.internal.on('end', () => this.#endHandler(socket));
-    socket.internal.on('data', (data) => this.#dataHandler(socket, data));
+      socket.internal.on('end', () => this.#endHandler(socket));
+      socket.internal.on('data', (data) => this.#dataHandler(socket, data));
+    } catch (error) {
+      console.log(
+        colors.brightRed(`(${socket.addr}:${socket.port}) disconnected abruptly.`),
+      );
+    }
   }
 
   #listen = () => {
